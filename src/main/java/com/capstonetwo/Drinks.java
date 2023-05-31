0f6b495a4da7e9052bdc48f4d087b9cf48a45cd2
Melissa Guzman <melissaaa.24.guz@gmail.com>
2023-05-31 14:22:07 -0400

    first drinks

--- src/main/java/com/capstonetwo/Drinks.java
+++ src/main/java/com/capstonetwo/Drinks.java
@@ -1,12 +1,23 @@
 package com.capstonetwo;
+import java.util.ArrayList;
 
 public class Drinks {
+    private String drinkType;
     private String size;
     private double cost;
-
-    public Drinks(String size, double cost) {
+    //co?nstructor yea
+    public Drinks(String drinkType, String size, double cost) {
+        this.drinkType = drinkType;
         this.size = size;
         this.cost = cost;
+    }
+    //generate geter setter
+    public String getDrinkType() {
+        return drinkType;
+    }
+
+    public void setDrinkType(String drinkType) {
+        this.drinkType = drinkType;
     }
 
     public String getSize() {
@@ -24,4 +35,42 @@ public class Drinks {
     public void setCost(double cost) {
         this.cost = cost;
     }
+
+    public static class DrinkMenu {
+        // Moved the drinks menu to create the list of drinks
+
+        private ArrayList<String> drinks;
+
+        public DrinkMenu() {
+            drinks = new ArrayList<>();
+        }
+
+        public void addDrink(String drink) {
+            drinks.add(drink);
+        }
+
+        public void displayMenu() {
+            System.out.println("Drink Menu:");
+            for (String drink : drinks) {
+                System.out.println(drink);
+            }
+        }
+
+        public static void main(String[] args) {
+            DrinkMenu menu = new DrinkMenu();
+            menu.addDrink("Cola");
+            menu.addDrink("Sprite");
+            menu.addDrink("Iced Tea");
+            menu.addDrink("Sparking Water");
+            menu.displayMenu();
+        }
+    }
+
+    // i removed the setCalculateDrinkPrice method, it wasnt working so i modified
+    public void displayDrinkOrder() {
+        System.out.println("What drink would you like? " + drinkType);
+        System.out.println("What size of cup? " + size);
+        System.out.println("This is the price of the cup: $" + cost);
+    }
 }
+
