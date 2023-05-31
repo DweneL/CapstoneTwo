a4fe350ea92e548e891ecc8f7f895b63f453f585
Dwene Louis <Dwenegt@yahoo.com>
2023-05-31 16:05:13 -0400

    Added some methods to the sandwich classes

--- src/main/java/com/capstonetwo/Sandwich.java
+++ src/main/java/com/capstonetwo/Sandwich.java
@@ -5,21 +5,105 @@ public class Sandwich {
     private Meat meat;
     private Cheese cheese;
     private Toppings topping;
-    private String sauce;
+    private Sauces sauce;
     private String side;
     private int sandwichSize;
-    private float price;
+    private double price;
+    private boolean isCheeseAdded = false;
+    private boolean isMeatAdded = false;
+    private boolean isToppingsAdded = false;
+    private boolean isExtraMeatAdded = false;
+    private boolean isExtraCheeseAdded = false;
+    private boolean isToasted = false;
 
-    public Sandwich(Bread bread, Meat meat, Cheese cheese, Toppings topping, String sauce, String side, int sandwichSize, float price) {
-        this.bread = bread;
-        this.meat = meat;
-        this.cheese = cheese;
-        this.topping = topping;
-        this.sauce = sauce;
-        this.side = side;
+    public Sandwich(int sandwichSize, double price) {
         this.sandwichSize = sandwichSize;
         this.price = price;
     }
+
+
+
+    public void addBread(Bread bread){
+        if (sandwichSize == 4){
+            price = 5.50;
+        }
+        if (sandwichSize == 8){
+            price = 7.00;
+        }
+        if (sandwichSize == 12){
+            price = 8.50;
+        } else {
+            System.out.println("The size you have chosen is not available");
+        }
+
+    }
+
+    public void addMeat(Meat meat){
+        if (isMeatAdded == true && sandwichSize == 4){
+            this.price += 1.00;
+        } if (isMeatAdded == true && sandwichSize == 8){
+            this.price += 2.00;
+        } if (isMeatAdded == true && sandwichSize == 12){
+            this.price += 3.00;
+        }
+    }
+
+    public void addExtraMeat(Meat meat){
+        if (isExtraMeatAdded == true && sandwichSize == 4){
+            this.price += .50;
+        } if (isExtraMeatAdded == true && sandwichSize == 8){
+           this.price +=  1.00;
+        } if (isExtraMeatAdded == true && sandwichSize == 12){
+            this.price += 1.50;
+        }
+
+    }
+
+    public void addCheese(Cheese cheese){
+        if (isMeatAdded == true && sandwichSize == 4){
+            this.price += .75;
+        } if (isMeatAdded == true && sandwichSize == 8){
+            this.price += 1.50;
+        } if (isMeatAdded == true && sandwichSize == 12){
+            this.price += 2.25;
+        }
+    }
+
+    public void addExtraCheese(Cheese cheese){
+        if(isExtraCheeseAdded == true && sandwichSize == 4){
+            this.price += .30;
+        } if (isExtraCheeseAdded == true && sandwichSize == 8){
+            this.price += .60;
+        } if (isExtraCheeseAdded == true && sandwichSize == 12){
+            this.price += .90;
+        }
+
+    }
+
+    public void addToppings (Toppings topping){
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public Bread getBread() {
         return bread;
@@ -53,11 +137,11 @@ public class Sandwich {
         this.topping = topping;
     }
 
-    public String getSauce() {
+    public Sauces getSauce() {
         return sauce;
     }
 
-    public void setSauce(String sauce) {
+    public void setSauce(Sauces sauce) {
         this.sauce = sauce;
     }
 
@@ -77,11 +161,11 @@ public class Sandwich {
         this.sandwichSize = sandwichSize;
     }
 
-    public float getPrice() {
+    public double getPrice() {
         return price;
     }
 
-    public void setPrice(float price) {
+    public void setPrice(double price) {
         this.price = price;
     }
 }
