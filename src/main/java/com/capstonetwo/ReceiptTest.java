89756f349824172d025a81b51fb698ea8795c277
rial tulloch <rial.tulloch@gmail.com>
2023-06-01 14:47:23 -0400

    receipt

---
+++ src/main/java/com/capstonetwo/ReceiptTest.java
@@ -0,0 +1,21 @@
+package com.capstonetwo;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReceiptTest {
+    @Test
+    public void printReceiptTest(){
+        Sandwich sandwich = new Sandwich(Bread.WHITE, Meat.BACON, Cheese.PROVOLONE, Toppings.JALAPENOS, Sauces.RANCH,"Mac",
+                4, 0, true,true,true,true, true, true);
+        List<Sandwich> sandwichList = new ArrayList<>();
+        sandwichList.add(sandwich);
+        Receipt receipt = new Receipt(sandwichList);
+        receipt.printReceipt();
+    }
+
+}
